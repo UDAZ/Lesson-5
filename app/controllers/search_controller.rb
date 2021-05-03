8fca62d5b4107af9346e7eec292238c7504397a1
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-03 08:20:08 +0000

    add search action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,10 @@
 class SearchController < ApplicationController
     def search
-        
+        @model = params["search"]["model"]
+        @value = params["search"]["value"]
+        @how = params["search"]["how"]
+        @datas = search_for(@how, @model, @value)
     end
+    
+    private
 end
