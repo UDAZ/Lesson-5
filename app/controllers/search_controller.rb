785b1295c5ba766888be355946f823eecf4b5a1c
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-03 09:20:00 +0000

    add partical

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -30,5 +30,26 @@ class SearchController < ApplicationController
         elsif model == 'book'
             Book.where("title LIKE ?", "%#{value}")
         end
-    end 
+    end
+    
+    def partical(model, value)
+        if model == 'user'
+            User.where("name LIKE ?", "%#{value}%")
+        elsif model == 'book'
+            Book.where("title LIKE ?", "%#{value}%")
+        end
+    end
+    
+    def search_for(how, model, value)
+        case how
+        when 'match'
+            match(model, value)
+        when 'forward'
+            forward(model, value)
+        when 'backward'
+            backward(model, value)
+        when 'partical'
+            partical(model, value)
+        end
+    end
 end
